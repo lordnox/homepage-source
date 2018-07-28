56e6f083e715adaeeb2b02c319cdfccb04ded36b
Tobias K <nox@raynode.de>
2018-07-29 00:53:22 +0200

    have to return next function

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,7 @@ const createHandler: (options: CreateHandlerOptions) => handler = fckCreateHandl
 import { log } from 'logger'
 
 const app = new Koa()
+
 const router = new Router()
 const port = process.env.PORT || 3000
 const handler = createHandler({
@@ -24,6 +25,7 @@ const handler = createHandler({
 
 router.post('/webhooks/*', (ctx, next) => {
   handler(ctx.req, ctx.res, err => ctx.throw(404, err))
+  return next()
 })
 
 handler.on('error', err => {
