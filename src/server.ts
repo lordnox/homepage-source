904ea1632d427604f264cad293f0fc8f80f83a46
Tobias K <nox@raynode.de>
2018-07-28 15:46:38 +0200

    added fallback for all other paths

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 
-import * as Koa from 'koa'
-import * as serve from 'koa-static'
-import { log } from 'logger'
 import { join } from 'path'
+
+import * as Koa from 'koa'
+import * as bodyParser from 'koa-bodyparser'
+import * as serve from 'koa-static'
+
+import { log } from 'logger'
 
 const port = process.env.PORT || 3000
 
 const app = new Koa()
 app.use(serve(join(__dirname, '..', 'public')))
 
+app.use(bodyParser())
 app.use(async ctx => {
-  ctx.body = 'Hello World!'
+  log(ctx.request.body)
+  log(ctx.request.url)
+  ctx.body = 'ok'
 })
 
 app.listen(port, () => {
